77b3337ae435bb49b47c9456b5a1fd45ebe98d99
Mohammadazim Patel <mohammadazimpatel05@gmail.com>
2026-09-16 08:52:13 +0530

    Fix: Removed root files and unwanted configs

--- .cargo/registry/src/index.crates.io-1949cf8c6b5b557f/purl-0.1.6/src/qualifiers/well_known/gem.rs
+++
@@ -1,7 +0,0 @@
-//! Known qualifier types for [gem].
-//!
-//! [gem]: https://github.com/package-url/purl-spec/blob/master/PURL-TYPES.rst#gem
-
-use super::str_ref_qualifier;
-
-str_ref_qualifier!(Platform, "platform", "platform");
--- .cargo/registry/src/index.crates.io-1949cf8c6b5b557f/purl-0.1.6/src/qualifiers/well_known/maven.rs
+++
@@ -1,8 +0,0 @@
-//! Known qualifier types for [maven].
-//!
-//! [maven]: https://github.com/package-url/purl-spec/blob/master/PURL-TYPES.rst#maven
-
-use super::str_ref_qualifier;
-
-str_ref_qualifier!(Classifier, "classifier", "classifier");
-str_ref_qualifier!(Type, "type", "type");
